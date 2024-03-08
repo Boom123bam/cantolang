305b4992b2fde24a3257b599033cf3eba659aeb1
Brandon <brandontang0325@gmail.com>
2024-03-08 07:59:47 +0000

    Add support for file arg

--- main.go
+++ main.go
@@ -1,10 +1,33 @@
 package main
 
 import (
+	"cantolang/evaluator"
+	"cantolang/lexer"
+	"cantolang/object"
+	"cantolang/parser"
 	"cantolang/repl"
+	"fmt"
 	"os"
 )
 
 func main() {
-	repl.Start(os.Stdin, os.Stdout)
+	switch len(os.Args) {
+	case 1:
+		repl.Start(os.Stdin, os.Stdout)
+	case 2:
+		filename := os.Args[1]
+		data, err := os.ReadFile(filename)
+		if err != nil {
+			fmt.Println("Error reading file:", err)
+			return
+		}
+		// Convert the byte slice to a string
+		input := string(data)
+		l := lexer.New(input)
+		p := parser.New(l)
+		program := p.ParseProgram()
+		evaluator.Eval(program, object.NewEnvironment(nil))
+	default:
+		fmt.Println("usage: go run main.go (filename)")
+	}
 }
